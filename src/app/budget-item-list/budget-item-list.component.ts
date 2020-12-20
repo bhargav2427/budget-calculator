5c3d27eecf697efe7983f865a4f853153e203068
bhargav2427 <akhani.bharga@gmail.com>
2020-12-20 18:35:37 +0530

    Dialog box added

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { BudgetItem } from 'src/shared/models/budget-list.model';
+import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
 
 @Component({
   selector: 'app-budget-item-list',
@@ -10,7 +12,7 @@ export class BudgetItemListComponent implements OnInit {
 
   @Input() budgetItems: BudgetItem[];
   @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
@@ -18,5 +20,24 @@ export class BudgetItemListComponent implements OnInit {
 onDeleteButtonClicked(item: BudgetItem) {
    this.delete.emit();
 }
+
+onCardClicked(item: BudgetItem) {
+  const dialogRef = this.dialog.open(EditItemModalComponent, 
+    {
+      width: '580px',
+      data: item
+    });
+
+
+  dialogRef.afterClosed().subscribe(result => {
+    if(result) {
+      this.budgetItems[this.budgetItems.indexOf(item)] = result;
+    }
+  })
+
+
+}
+
+
 
 }
